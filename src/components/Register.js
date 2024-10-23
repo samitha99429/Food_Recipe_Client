39d5be50bf1849b4f5cb7d06dd4f0c3fdebdd275
samitha senevirathne <88784541+samitha99429@users.noreply.github.com>
2024-10-23 16:01:04 +0530

    done
    done

--- src/components/Register.js
+++ src/components/Register.js
@@ -36,7 +36,7 @@ const Register = () => {
     setErrorMessage('');
 
     try {
-      await axios.post('http://localhost:3001/api/auth/register', { firstname, lastname, email, phonenumber, password });
+      await axios.post('https://food-recipe-api-mu.vercel.app/auth/register', { firstname, lastname, email, phonenumber, password });
       navigate('/login');
     } catch (err) {
       console.error(err);
